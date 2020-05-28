60101858c233e4f0e83d38c4f8733c71187a1639
Martin Kostelanský <33228093+kostelansky17@users.noreply.github.com>
2020-05-28 13:49:04 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='naolution',
       version='0.1',
-      description='The funniest joke in the world',
+      description='Cnns and robot NAO',
       url='https://github.com/kostelansky17/NAOlution',
       author='Martin Kostelansky',
       author_email='mkostelansky@gmail.com',
